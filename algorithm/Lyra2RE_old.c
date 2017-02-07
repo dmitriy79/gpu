cc688c768ecf0190764bf6643dec1482b01d8fc7
dmitriy79 <sinoptik79@inbox.ru>
2017-02-08 02:46:01 +1000

    Update Lyra2RE_old.c

--- algorithm/Lyra2RE_old.c
+++ algorithm/Lyra2RE_old.c
@@ -1,30 +1,3 @@
-/*-
- * Copyright 2014 James Lovejoy
- * Copyright 2014 phm
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
 #include "config.h"
 #include "miner.h"
 
@@ -55,9 +28,6 @@ be32enc_vect(uint32_t *dst, const uint32_t *src, uint32_t len)
 inline void lyra2rehash_old(void *state, const void *input)
 {
     sph_blake256_context     ctx_blake;
-    sph_groestl256_context   ctx_groestl;
-    sph_keccak256_context    ctx_keccak;
-    sph_skein256_context     ctx_skein;
 
     uint32_t hashA[8], hashB[8];
 
@@ -65,28 +35,9 @@ inline void lyra2rehash_old(void *state, const void *input)
     sph_blake256 (&ctx_blake, input, 80);
     sph_blake256_close (&ctx_blake, hashA);
 
+	LYRA2O(hashB, 32, hashA, 32, hashA, 32, 8, 8, 8);
 
-
-
-    sph_keccak256_init(&ctx_keccak);
-    sph_keccak256 (&ctx_keccak,hashA, 32);
-    sph_keccak256_close(&ctx_keccak, hashB);
-
-	LYRA2O(hashA, 32, hashB, 32, hashB, 32, 1, 8, 8);
-
-
-	sph_skein256_init(&ctx_skein);
-    sph_skein256 (&ctx_skein, hashA, 32);
-    sph_skein256_close(&ctx_skein, hashB);
-
-
-    sph_groestl256_init(&ctx_groestl);
-    sph_groestl256 (&ctx_groestl, hashB, 32);
-    sph_groestl256_close(&ctx_groestl, hashA);
-
-//printf("cpu hash %08x %08x %08x %08x\n",hashA[0],hashA[1],hashA[2],hashA[3]);
-
-	memcpy(state, hashA, 32);
+	memcpy(state, hashB, 32);
 }
 
 static const uint32_t diff1targ = 0x0000ffff;
